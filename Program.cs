c4817fa42d26e8c3ed08f01ab97b22e084a447dc
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-03-01 07:55:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+            static void Main()
+            {
+                int count = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    if (InputNumber(Console.ReadLine()))
+                        count++;
+                }
+                Console.WriteLine(count);
+            }
+
+            static bool InputNumber(string str)
+            {
+                long num = long.Parse(str);
+                long b = 0;
+                /*str = str + ".";
+                int len = str.IndexOf('.')-1;*/
+                long number = num;
+                while (number > 0)
+                {
+                    b = b * 10 + number % 10;
+                    number /= 10;
+                }
+                if (num == b) return true;
+                return false;
+            }
+
+        
+    }
+}
